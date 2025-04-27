a2117196af4108ba6a24d6a5d8241bc260fc1dda
Wy155 <yawwy-wp22@student.tarc.edu.my>
2025-04-27 10:12:17 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
